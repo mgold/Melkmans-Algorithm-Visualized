1d10c347fdc3323ea8ba79bc35f116de2c90a6d9
Max Goldstein <maxgoldstein1@gmail.com>
2015-01-07 00:04:52 -0500

    crib line intersection code from stack overflow

--- melkman.js
+++ melkman.js
@@ -22,25 +22,38 @@ function dist2(p0, p1){
     return dx*dx + dy*dy;
 }
 
-// Do the lines p0p1 and p2p3 intersect?
-function intersect(p0, p1, p2, p3){
-    var r0_x_min = Math.min(p0[0], p1[0]),
-        r0_x_max = Math.max(p0[0], p1[0]),
-        r1_x_min = Math.min(p2[0], p3[0]),
-        r1_x_max = Math.max(p2[0], p3[0]),
-        r0_y_min = Math.min(p0[1], p1[1]),
-        r0_y_max = Math.max(p0[1], p1[1]),
-        r1_y_min = Math.min(p2[1], p3[1]),
-        r1_y_max = Math.max(p2[1], p3[1]),
-        bb_not_intersect = (r0_x_max < r1_x_min || r0_x_min > r1_x_max) &&
-                       (r0_y_max < r1_y_min || r0_y_min > r1_y_max)
+// http://stackoverflow.com/questions/9043805
+function intersect(p0, p1, p2, p3) {
+    var x1=p0[0], y1=p0[1], x2=p1[0], y2=p1[1],
+        x3=p2[0], y3=p2[1], x4=p3[0], y4=p3[1];
 
-    if (bb_not_intersect) return false;
-
-    var dir0 = leftTurn(p2, p0, p1),
-        dir1 = leftTurn(p3, p0, p1)
-
-    return dir0 != dir1;
+    var x=((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4));
+    var y=((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4));
+    if (isNaN(x)||isNaN(y)) {
+        return false;
+    } else {
+        if (x1>=x2) {
+            if (!(x2<=x&&x<=x1)) {return false;}
+        } else {
+            if (!(x1<=x&&x<=x2)) {return false;}
+        }
+        if (y1>=y2) {
+            if (!(y2<=y&&y<=y1)) {return false;}
+        } else {
+            if (!(y1<=y&&y<=y2)) {return false;}
+        }
+        if (x3>=x4) {
+            if (!(x4<=x&&x<=x3)) {return false;}
+        } else {
+            if (!(x3<=x&&x<=x4)) {return false;}
+        }
+        if (y3>=y4) {
+            if (!(y4<=y&&y<=y3)) {return false;}
+        } else {
+            if (!(y3<=y&&y<=y4)) {return false;}
+        }
+    }
+    return true;
 }
 
 function intersectsAny(p0, p1){
@@ -83,10 +96,11 @@ svg.on("click", function(){
     var pos = d3.mouse(svg.node())
     if (points.length > 0){
         var prev = points[points.length-1]
-        if (intersectsAny(prev, pos)){ return;}
         if (dist2(points[0], pos) < 25){ //complete circuit
             line(prev, points[0])
             console.log("completed circuit")
+            return;
+        }else if (intersectsAny(prev, pos)){
             return;
         }else{
             line(prev, pos);
