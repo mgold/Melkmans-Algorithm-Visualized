12ed56fbc3d9a1fd88d1596602c6ce708e345ffd
Max Goldstein <maxgoldstein1@gmail.com>
2015-01-25 19:06:15 -0500

    Use a package for line intersection

--- melkman.js
+++ melkman.js
@@ -1,6 +1,7 @@
 var Deque = require("collections/deque")
 var convexHull = require("quick-hull-2d")
 var _visibility = require("vishull2d")
+var intersect = require("segseg")
 var explanations = require("./explanation")
 
 function visibility(pts, cen){
@@ -32,46 +33,6 @@ function rightTurn(p0, p1, p2){
         c = p2[0] - p1[0],
         d = p2[1] - p1[1];
     return a*d - b*c > 0.001;
-}
-
-function dist2(p0, p1){
-    var dx = p1[0] - p0[0],
-        dy = p1[1] - p0[1];
-    return dx*dx + dy*dy;
-}
-
-// http://stackoverflow.com/questions/9043805
-function intersect(p0, p1, p2, p3) {
-    var x1=p0[0], y1=p0[1], x2=p1[0], y2=p1[1],
-        x3=p2[0], y3=p2[1], x4=p3[0], y4=p3[1];
-
-    var x=((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4));
-    var y=((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4));
-    if (isNaN(x)||isNaN(y)) {
-        return false;
-    } else {
-        if (x1>=x2) {
-            if (!(x2<=x&&x<=x1)) {return false;}
-        } else {
-            if (!(x1<=x&&x<=x2)) {return false;}
-        }
-        if (y1>=y2) {
-            if (!(y2<=y&&y<=y1)) {return false;}
-        } else {
-            if (!(y1<=y&&y<=y2)) {return false;}
-        }
-        if (x3>=x4) {
-            if (!(x4<=x&&x<=x3)) {return false;}
-        } else {
-            if (!(x3<=x&&x<=x4)) {return false;}
-        }
-        if (y3>=y4) {
-            if (!(y4<=y&&y<=y3)) {return false;}
-        } else {
-            if (!(y3<=y&&y<=y4)) {return false;}
-        }
-    }
-    return true;
 }
 
 function intersectsAny(p0, p1){
